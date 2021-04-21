bbd831fa2ba74100797d203b8a9e8ccca99e4420
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-20 21:04:30 -0600

    Update util.py

--- util.py
+++ util.py
@@ -1,12 +1,15 @@
 import numpy as np
 import math
 
+# This gets the coordinates for the damaged areas on the config space map.
 def get_bounds():
     raise NotImplementedError()
-    
+   
+# This takes the output from the autonomous mapper and converts it to config space map.
 def get_config_space():
     raise NotImplementedError()
-    
+
+# This takes the list of boundaries for damaged areas and decides where to spawn the manipulator arm and repair materials
 def get_spawn_location():
     raise NotImplementedError()
     
