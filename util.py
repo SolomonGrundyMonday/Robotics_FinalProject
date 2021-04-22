47d432256fd38ec587efc8f7767b2065e15a2631
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-21 18:21:40 -0600

    Add files via upload

--- util.py
+++ util.py
@@ -1,15 +1,26 @@
 import numpy as np
+from matplotlib import pyplot as plt 
 import math
+from scipy.signal import convolve2d
 
-# This gets the coordinates for the damaged areas on the config space map.
 def get_bounds():
     raise NotImplementedError()
-   
-# This takes the output from the autonomous mapper and converts it to config space map.
+    
 def get_config_space():
-    raise NotImplementedError()
+    map = np.load('map.npy')
+    kernel = np.ones((3, 3))
+    config_space = convolve2d(map, kernel, mode='same')
 
-# This takes the list of boundaries for damaged areas and decides where to spawn the manipulator arm and repair materials
+    for i in range(0,400):
+        for j in range(0,400):
+            if(config_space[i][j] >= 600):
+                config_space[i][j] = 1
+                
+    plt.imshow(config_space, cmap='gray')
+    plt.show()
+    return config_space
+    
 def get_spawn_location():
     raise NotImplementedError()
     
+get_config_space()
