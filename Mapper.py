51ed7c6402c0512da43cb5cc35cf537977ade10a
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-26 02:33:53 -0600

    Updated Mapper code and output

--- Mapper.py
+++ Mapper.py
@@ -15,11 +15,13 @@ MAX_LIDAR_ANGLE = math.radians(240)
 LIDAR_SENSOR_MAX_RANGE = 5.5
 MAP_HEIGHT = 400
 MAP_WIDTH = 400
-DAMAGE_THRESHOLD = .74
+DAMAGE_THRESHOLD = .95
 INCREMENT_CMAP = 0.0005
 NOISE_THRESHOLD = 1.0
 FORTY_FIVE_DEGREES = math.pi/4
 NINETY_DEGREES = math.pi/2
+SINUSOIDAL_FUNCTION_MULTIPLIER = 2.0 * math.pi * 0.5
+mode = 'mapping'
 
 # create the Robot instance.
 robot = Supervisor()
@@ -50,9 +52,9 @@ compass.enable(timestep)
 display = robot.getDevice('display')
 lidarReadings = []
 lidarOffsetAngles = np.linspace(-MAX_LIDAR_ANGLE/2, MAX_LIDAR_ANGLE/2, ANGLE_BINS)
+lidarOffsetAngles = lidarOffsetAngles[83:len(lidarOffsetAngles)-83]
     
 map = np.zeros(shape=(MAP_WIDTH, MAP_HEIGHT))
-frequency = 0.5
 
 # amplitude (radians)
 baseAmp = 0.25
@@ -77,76 +79,84 @@ goal_theta = math.pi
 # Main controller loop:
 while robot.step(timestep) != -1:
     
-    # Retrieve the pose coordinates from the GPS and Compass units
-    time = robot.getTime()
-    coord = gps.getValues()
-    n = compass.getValues()
-    pose_x = coord[0]
-    pose_y = coord[2]
-    pose_theta = -((math.atan2(n[0], n[2])) - (NINETY_DEGREES))
-    bearing_err = pose_theta - goal_theta
+    if(mode == 'mapping'):
+        # Retrieve the pose coordinates from the GPS and Compass units
+        time = robot.getTime()
+        coord = gps.getValues()
+        n = compass.getValues()
+        pose_x = coord[0]
+        pose_y = coord[2]
+        pose_theta = -((math.atan2(n[0], n[2])) - (NINETY_DEGREES))
+        bearing_err = pose_theta - goal_theta
     
     
-    #print("bearing error: ", bearing_err, "goal theta: ", goal_theta)
-    #print("(x, y): (", pose_x, ", ", pose_y, ")")   
-    if abs(bearing_err) > 0.1 and pose_y < 7 and goal_theta != 0: #south           
-        for i in range(0, 9):#all the right legs
-            motors[i].setPosition(ampVector[i] * math.sin(2.0 * math.pi * frequency * time + phaseVector[i]) + offsetVector[i])
-        for i in range(9, 18):
-            motors[i].setPosition(ampVector[i] * math.sin(1.5 * math.pi * frequency * time - phaseVector[i]) + offsetVector[i])
-    elif abs(bearing_err) > 0.1 and pose_y > 93 and goal_theta != math.pi:    
-        for i in range(0, 9):#all the right legs
-            motors[i].setPosition(ampVector[i] * math.sin(0.8 * math.pi * frequency * time - phaseVector[i]) + offsetVector[i])
-        for i in range(9, 18):#all the left legs
-            motors[i].setPosition(ampVector[i] * math.sin(2.5 * math.pi * frequency * time + phaseVector[i]) + offsetVector[i])        
-    else:#go straight forward
+        #print("bearing error: ", bearing_err, "goal theta: ", goal_theta)
+        #print("(x, y): (", pose_x, ", ", pose_y, ")")   
+        if abs(bearing_err) > 0.2 and pose_y < 7 and goal_theta != 0: #south           
+            for i in range(0, 10):#all the right legs
+                motors[i].setPosition(ampVector[i] * math.sin(SINUSOIDAL_FUNCTION_MULTIPLIER * time + phaseVector[i]) + offsetVector[i])
+            for i in range(9, 18):
+                motors[i].setPosition(ampVector[i] * math.sin(-SINUSOIDAL_FUNCTION_MULTIPLIER * time + phaseVector[i]) + offsetVector[i])
+        elif abs(bearing_err) > 0.2 and pose_y > 93 and goal_theta != math.pi:    
+            for i in range(0, 10):#all the right legs
+                motors[i].setPosition(ampVector[i] * math.sin(-0.5*SINUSOIDAL_FUNCTION_MULTIPLIER * time + phaseVector[i]) + offsetVector[i])
+            for i in range(9, 18):#all the left legs
+                motors[i].setPosition(ampVector[i] * math.sin(1.5*SINUSOIDAL_FUNCTION_MULTIPLIER * time + phaseVector[i]) + offsetVector[i])        
+        else:#go straight forward
     
-        if pose_y < 7:
-            goal_theta = 0
-        elif pose_y > 93:
-            goal_theta = math.pi
-        for i in range(0, 18):  
-            motors[i].setPosition(ampVector[i] * math.sin(2.0 * math.pi * frequency * time + phaseVector[i]) + offsetVector[i])
+            if pose_y < 7:
+                goal_theta = 0
+            elif pose_y > 93:
+                goal_theta = math.pi
+            for i in range(0, 18):  
+                motors[i].setPosition(ampVector[i] * math.sin(SINUSOIDAL_FUNCTION_MULTIPLIER * time + phaseVector[i]) + offsetVector[i])
       
-    lidarReadings = lidar.getRangeImage()
+        lidarReadings = lidar.getRangeImage()
+        lidarReadings = lidarReadings[83:len(lidarReadings)-83]
     
-    # Print the mapping data onto the display for debugging purposes.  Remove Display code/module for final demonstration
-    for i, rho in enumerate(lidarReadings):
-        # Compute rho and alpha from the sensor data
-        alpha = lidarOffsetAngles[i]
-        if rho > LIDAR_SENSOR_MAX_RANGE:
-            rho = LIDAR_SENSOR_MAX_RANGE
+        # Print the mapping data onto the display for debugging purposes.  Remove Display code/module for final demonstration
+        for i, rho in enumerate(lidarReadings):
+            # Compute rho and alpha from the sensor data
+            alpha = lidarOffsetAngles[i]
+            if rho > LIDAR_SENSOR_MAX_RANGE:
+                rho = LIDAR_SENSOR_MAX_RANGE
             
-        # Compute the robot/world coordinates for sensor data
-        rx = math.cos(alpha)*rho
-        ry = -math.sin(alpha)*rho
-        wx = (math.cos(pose_theta)*rx - math.sin(pose_theta)*ry) + pose_x
-        wy = -(math.sin(pose_theta)*rx + math.cos(pose_theta)*ry) + pose_y
+            # Compute the robot/world coordinates for sensor data
+            rx = math.cos(alpha)*rho
+            ry = -math.sin(alpha)*rho
+            wx = math.cos(pose_theta)*rx - math.sin(pose_theta)*ry + pose_x
+            wy = -(math.sin(pose_theta)*rx + math.cos(pose_theta)*ry) + pose_y
 
-        # Draw to the display if the sensor output is within threshold
-        if rho > DAMAGE_THRESHOLD and alpha <= (FORTY_FIVE_DEGREES) and alpha >= -(FORTY_FIVE_DEGREES):
-            (pixel_x, pixel_y) = (400-int(wx*4), 400-int(wy*4))
+            # Draw to the display if the sensor output is within threshold
+            if rho > DAMAGE_THRESHOLD and alpha <= (FORTY_FIVE_DEGREES) and alpha >= -(FORTY_FIVE_DEGREES):
+                (pixel_x, pixel_y) = (400-int(wx*4), 400-int(wy*4))
             
-            # Ensure that the pixel to be drawn is within the bounds of the map
-            if(pixel_x >= 0 and pixel_x < MAP_WIDTH and pixel_y >= 0 and pixel_y < MAP_HEIGHT):
-                map[pixel_x][pixel_y] += INCREMENT_CMAP
+                # Ensure that the pixel to be drawn is within the bounds of the map
+                if(pixel_x >= 0 and pixel_x < MAP_WIDTH and pixel_y >= 0 and pixel_y < MAP_HEIGHT):
+                    map[pixel_x][pixel_y] += INCREMENT_CMAP
                 
-                # Filter out sensor noise
-                if(map[pixel_x][pixel_y] > NOISE_THRESHOLD):
-                    map[pixel_x][pixel_y] = NOISE_THRESHOLD
+                    # Filter out sensor noise
+                    if(map[pixel_x][pixel_y] > NOISE_THRESHOLD):
+                        map[pixel_x][pixel_y] = NOISE_THRESHOLD
                     
-                # Print sensor data above threshold after filtering noise to the display
-                g = int(map[pixel_x][pixel_y]*255)
-                g = int((g*256**2+g*256+g))
-                display.setColor(g)
-                display.drawPixel(pixel_x, pixel_y)
+                    #display.setColor(0xFF0000)
+                    #display.drawPixel(pixel_x, pixel_y)
+                    # Print sensor data above threshold after filtering noise to the display
+                    g = int(map[pixel_x][pixel_y]*255)
+                    g = int((g*256**2+g*256+g))
+                    display.setColor(g)
+                    display.drawPixel(pixel_x, pixel_y)
                 
-    if np.isclose(pose_x, 5, 0.4) and np.isclose(pose_y, 5, 0.4):
-         robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
-         np.save('map', map)
-         # Do stuff here to compute the c-spoce map and spawn in the manipulator arm
-         # and repair equipment to the appropriate locations
-         plt.imshow(map, cmap='gray')
-         plt.show()
-         robot.simulationSetMode(Supervisor.SIMULATION_MODE_REAL_TIME)
+        if np.isclose(pose_x, 5, 0.4) and np.isclose(pose_y, 5, 0.4):
+             robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
+             np.save('map', map)
+             # Do stuff here to compute the c-spoce map and spawn in the manipulator arm
+             # and repair equipment to the appropriate locations
+             plt.imshow(map, cmap='gray')
+             plt.show()
+             mode = 'repair'
+             robot.simulationSetMode(Supervisor.SIMULATION_MODE_REAL_TIME)
                 
+    elif (mode == 'repair'):
+        continue
+        # Code to spawn in and compute inverse kinematics of manipulator arm here.
