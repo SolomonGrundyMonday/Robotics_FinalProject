b220f55d38521c6e8d71380e7cce64fe7dc94e66
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-26 14:51:32 -0600

    New update

--- util.py
+++ util.py
@@ -2,8 +2,30 @@ import numpy as np
 import math
 from scipy.signal import convolve2d
 
-def get_bounds():
-    raise NotImplementedError()
+def get_bounds(config_space):
+    #print('Made it to line 6, get_bounds')
+    areas = []
+    width_index = []
+    
+    a = 0
+    for i in range(0, 400):
+        #print('First for loop, get_bounds')
+        same_area = False
+        for j in range(0, 400):
+            if(config_space[i][j] == 1):               
+                a += 1
+                width_index.append(j)
+                
+        if len(width_index) != 0:        
+            for k in width_index:
+                if(config_space[i][k] == 1):
+                    same_area = True 
+                 
+        if(same_area == False and a != 0):
+            areas.append(a)
+            a = 0
+             
+    return areas 
     
 def get_config_space(map):
     kernel = np.ones((15, 15))
