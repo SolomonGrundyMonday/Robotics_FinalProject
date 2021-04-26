3a28be96b79b1a5a60e6d1b6ffbd48a437284030
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-26 13:45:25 -0600

    Updated utility functions for supervisor

--- util.py
+++ util.py
@@ -1,26 +1,22 @@
-import numpy as np
-from matplotlib import pyplot as plt 
+import numpy as np 
 import math
 from scipy.signal import convolve2d
 
 def get_bounds():
     raise NotImplementedError()
     
-def get_config_space():
-    map = np.load('map.npy')
-    kernel = np.ones((3, 3))
+def get_config_space(map):
+    kernel = np.ones((15, 15))
     config_space = convolve2d(map, kernel, mode='same')
 
     for i in range(0,400):
         for j in range(0,400):
-            if(config_space[i][j] >= 600):
+            if(config_space[i][j] >= 400):
                 config_space[i][j] = 1
                 
-    plt.imshow(config_space, cmap='gray')
-    plt.show()
+    
     return config_space
     
 def get_spawn_location():
     raise NotImplementedError()
     
-get_config_space()
