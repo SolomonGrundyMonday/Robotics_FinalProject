93c3173a37ae7b605e5d3f266ff70b691d2e7aef
VVcattie <48469990+VVcattie@users.noreply.github.com>
2021-04-27 17:04:56 -0600

    Updated Gripper

--- Gripper.py
+++ Gripper.py
@@ -1,34 +1,36 @@
 from controller import Motor, Robot
 
-MIN = 0.0
-MAX = 0.025
 OFFSET = 0.021
 
-fingers = []
+class Gripper:
 
-def init(robot):
-    fingers.append(robot.getDevice('finger1'))
-    fingers.append(robot.getDevice('finger2'))
-
-    for f in fingers:
-        f.setVelocity(0.03)
-
-def grip():
-    for f in fingers:
-        f.setPosition(MIN)
-
-def release():
-    for f in fingers:
-        f.setPosition(MAX)
-
-def set_gap(gap):
-    v = 0.5 * (gap - OFFSET)
-    a = MIN
-    b = MAX
-    if v > MIN:
-        v = MIN
-    else:
-        if v < MAX:
-            v = MAX
-    for f in fingers:
-        f.setPosition(v)
+    def __init__(self):
+        self.MIN = 0.0   #doubt
+        self.MAX = 0.025 #doubt
+        self.fingers = []
+        self.fingers.append(robot.getDevice('finger1'))
+        self.fingers.append(robot.getDevice('finger2'))
+    
+        for f in fingers:
+            self.f.setVelocity(0.03)
+    
+    def grip(self):
+        for f in fingers:
+            self.f.setPosition(MIN)
+    
+    def release(self):
+        for f in fingers:
+            self.f.setPosition(MAX)
+    
+    def set_gap(self,gap):
+        v = 0.5 * (gap - OFFSET)
+        a = MIN
+        b = MAX
+        if v > MIN:
+            v = MIN
+        else:
+            if v < MAX:
+                v = MAX
+        for f in fingers:
+            self.f.setPosition(v)
+    
