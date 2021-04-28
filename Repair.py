fb1fa099dc4269e704c5861e03b8f8c5210f114b
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-04-28 00:51:32 -0600

    Initial attempt at feedback control

--- Repair.py
+++ Repair.py
@@ -8,47 +8,86 @@ import math
 robot = Supervisor()
 timestep = int(robot.getBasicTimeStep())
 
-print('initializing base')
 base = Base(robot)
-print('initializing arm')
 arm = Arm(robot)
-print('initializing gripper')
 gripper = Gripper(robot)
 
-print('Initialized all components!')
 keyboard = robot.getKeyboard()
 keyboard.enable(timestep)
 
-    
-print('Emtering main control loop!')
+compass = robot.getDevice('compass')
+compass.enable(timestep)
+
+gps = robot.getDevice('gps')
+gps.enable(timestep)
+
+waypoints = [(22.3, 24.7), (22.3, 26.2), (28.1, 26.4), (28.8, 25.3)]
+current_waypoint = waypoints.pop(0)
+have_block = False
 
 while (robot.step(timestep) != -1):
 
-    c = keyboard.getKey()
-    if ((c >= 0)):
-            # if c == WB_KEYBOARD_END:
-                #do nothing
-        if c == Keyboard.END:
-            arm.arm_reset()
-        elif c == Keyboard.PAGEUP:
-            gripper.grip()
-        elif c == Keyboard.PAGEDOWN:
-            gripper.release()
-        elif c == Keyboard.UP + Keyboard.SHIFT:
-            arm.increase_height()
-        elif c == Keyboard.DOWN + Keyboard.SHIFT:
-            arm.decrease_height()
-        elif c == Keyboard.RIGHT + Keyboard.SHIFT:
-            arm.increase_orientation()
-        elif c == Keyboard.LEFT + Keyboard.SHIFT:
-            arm.decrease_orientation()
-        elif c == Keyboard.UP:
-            base.base_forwards()
-        elif c == Keyboard.DOWN:
-            base.base_backwards()
-        elif c == Keyboard.LEFT:
-            base.base_turn_left()
-        elif c== Keyboard.RIGHT:
-            base.base_turn_right()
-        elif c == Keyboard.HOME:
-            base.base_stop()
+    coord = gps.getValues()
+    bearing = compass.getValues()
+    pose_x = coord[0]
+    pose_y = coord[2]
+    pose_theta = -((math.atan2(bearing[0], bearing[2]))-1.5708)
+    
+    bearing_error = pose_theta - math.atan2(current_waypoint[1] - pose_y, current_waypoint[0] - pose_x)
+    dist_error = dist_err = math.sqrt(math.pow(pose_x - current_waypoint[0], 2) + math.pow(pose_y - current_waypoint[1], 2))
+    
+    if dist_error <= 0.1:
+        current_waypoint = waypoints.pop(0)
+        
+    x_gain = 1.5
+    theta_gain = 3.3
+    x_prime = dist_error * x_gain
+    theta_prime = bearing_error * theta_gain
+    
+    if(bearing_error > 0.5):
+        vR = theta_prime + theta_gain
+        vL = -theta_prime - theta_gain
+        base.base_turn_right(vR, vL)
+    elif(bearing_error < -0.5):
+        vR = -theta_prime - theta_gain
+        vL = theta_prime + theta_gain
+        base.base_turn_left(vR, vL)
+    elif (not waypoints and dist_error < 0.1):
+        base.base_stop()
+    else:
+        v = x_prime + x_gain
+        base.base_forwards(v)
+        
+     
+    
+    print('Current Waypoint: ', current_waypoint, ' Current Location: ', coord)
+    print('Bearing Error: ', bearing_error, ' DistanceError: ', dist_error)
+    #c = keyboard.getKey()
+    #if ((c >= 0)):
+            
+       #if c == Keyboard.END:
+            #arm.arm_reset()
+        #elif c == Keyboard.PAGEUP:
+            #gripper.grip()
+        #elif c == Keyboard.PAGEDOWN:
+            #gripper.release()
+        #elif c == Keyboard.UP + Keyboard.SHIFT:
+            #arm.increase_height()
+        #elif c == Keyboard.DOWN + Keyboard.SHIFT:
+            #arm.decrease_height()
+        #elif c == Keyboard.RIGHT + Keyboard.SHIFT:
+            #arm.increase_orientation()
+        #elif c == Keyboard.LEFT + Keyboard.SHIFT:
+            #arm.decrease_orientation()
+        #elif c == Keyboard.UP:
+            #base.base_forwards()
+        #elif c == Keyboard.DOWN:
+            #base.base_backwards()
+        #elif c == Keyboard.LEFT:
+            #base.base_turn_left()
+        #elif c== Keyboard.RIGHT:
+            #base.base_turn_right()
+        #elif c == Keyboard.HOME:
+            #base.base_stop()
+        #else:
+            #arm.turn_wrist(-math.pi/2)
